e54a611732e441d685bc0bc1cde97f6a94a8c225
adam-saland <adam.saland@openfin.co>
2019-07-29 08:22:29 -0400

    update webpack config for flexibility

---
+++ src/launcher.js
@@ -0,0 +1,23 @@
+
+const openfinLauncher = require('hadouken-js-adapter');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const port = process.env.PORT || 8081;
+
+const configs = ['src/public/app.json'];
+
+const app = express();
+app.use(express.static('/'));
+
+http.createServer(app).listen(port, async function(){
+    console.log('Express server listening on port ' + port);
+    for (let i=0; i<configs.length; i++) {
+        const c = configs[i];
+        const confPath  = path.resolve(c);
+        openfinLauncher.launch({ manifestUrl: confPath})
+            .then(() => console.log(`app: ${c} started`))
+            .catch(err => console.error(`error running app: ${c}`, err));
+    }
+});
