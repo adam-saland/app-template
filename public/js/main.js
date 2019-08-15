3a883de2af196aa5b470abf502dbcf91e9136fe5
adam-saland <adam.saland@openfin.co>
2019-08-15 19:54:29 -0400

    find recursive

--- public/js/main.js
+++ public/js/main.js
@@ -5,68 +5,59 @@ document.addEventListener("DOMContentLoaded", async () => {
         init();
     } else {
         document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+            'The fin API is not available - you are probably running in a browser.';
     }
     const app = await fin.Application.getCurrent();
     const win = await fin.Window.getCurrent();
-    const webWin = fin.Window.getCurrentSync().getWebWindow().window;
-    function FindNext () {
-        let str = document.getElementById ("findField").value;
-        if (str == "") {
-            alert ("Please enter some text to search!");
+    //once the DOM has loaded and the OpenFin API is ready
+    window.addEventListener('keyup', (e) => {
+        if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+            console.log(document.getElementById('f1').getAttribute('visibility'));
+             
+        }
+    });
+    function findString(str) {
+        if (parseInt(navigator.appVersion) < 4) return;
+        let strFound;
+        if (window.find) {
+            // CODE FOR BROWSERS THAT SUPPORT window.find
+            strFound = self.find(str);
+            if (strFound && self.getSelection && !self.getSelection().anchorNode) {
+                strFound = self.find(str)
+            }
+            if (!strFound) {
+                strFound = self.find(str, 0, 1)
+                while (self.find(str, 0, 1)) continue
+            }
+        } else if (navigator.appName.indexOf("Microsoft") != -1) {
+            // EXPLORER-SPECIFIC CODE        
+            if (TRange != null) {
+                TRange.collapse(false)
+                strFound = TRange.findText(str)
+                if (strFound) TRange.select()
+            }
+            if (TRange == null || strFound == 0) {
+                TRange = self.document.body.createTextRange()
+                strFound = TRange.findText(str)
+                if (strFound) TRange.select()
+            }
+        } else if (navigator.appName == "Opera") {
+            alert("Opera browsers not supported, sorry...")
             return;
         }
-
-        let supported = false;
-        let found = false;
-        
-        if (webWin.find) {        // Firefox, Google Chrome, Safari
-            supported = true;
-                // if some content is selected, the start position of the search 
-                // will be the end position of the selection
-            found = fin.Window.getCurrentSync().getWebWindow().window.find (str);
-        }
-        else {
-            if (document.selection && document.selection.createRange) { // Internet Explorer, Opera before version 10.5
-                let textRange = document.selection.createRange ();
-                if (textRange.findText) {   // Internet Explorer
-                    supported = true;
-                        // if some content is selected, the start position of the search 
-                        // will be the position after the start position of the selection
-                    if (textRange.text.length > 0) {
-                        textRange.collapse (true);
-                        textRange.move ("character", 1);
-                    }
-
-                    found = textRange.findText (str);
-                    if (found) {
-                        textRange.select ();
-                    }
-                }
-            }
-        }
-
-        if (supported) {
-            if (!found) {
-                alert ("The following text was not found:\n" + str);
-            }
-        }
-        else {
-            alert ("Your browser does not support this example!");
-        }
-    }
-    //once the DOM has loaded and the OpenFin API is ready
-
+        if (!strFound) alert("String '" + str + "' not found!")
+        return;
+    };
     async function init() {
         //get a reference to the current Application.
-        const hotkey = 'CommandOrControl+F';
-
-        await fin.GlobalHotkey.register(hotkey, console.log)
 
         const ofVersion = document.querySelector('#of-version');
         ofVersion.innerText = await fin.System.getVersion();
-        const findText = document.querySelector("#find-text");
-        findText.addEventListener('click', FindNext)
+        document.getElementById('f1').onsubmit = function () {
+            findString(this.t1.value);
+            return false;
+        };
+
         //Only launch new windows from the main window.
         if (win.identity.name === app.identity.uuid) {
             //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
