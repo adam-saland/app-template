89ebcaf438e1d1f25130cd0213aa85d85a64553f
adam-saland <adam.saland@openfin.co>
2019-08-20 11:46:14 -0400

    dom el created

--- public/js/main.js
+++ public/js/main.js
@@ -7,15 +7,39 @@ if (typeof fin !== 'undefined') {
     document.querySelector('#of-version').innerText =
         'The fin API is not available - you are probably running in a browser.';
 }
-
 //once the DOM has loaded and the OpenFin API is ready
 async function init() {
     //get a reference to the current Application.
     const app = await fin.Application.getCurrent();
     const win = await fin.Window.getCurrent();
 
+
+
     const ofVersion = document.querySelector('#of-version');
     ofVersion.innerText = await fin.System.getVersion();
+
+
+    win.on('close-requested', async () => {
+        let btnHtmlStr = `
+            <div id="yesNo">
+            <h1> Are you sure you want to exit? </h1>
+            <span>
+                <button id="yes">Yes</button>
+            </span>
+            <span>
+                <button id="no">No</button>
+            </span>
+            </div>`
+        document.body.innerHTML += btnHtmlStr
+        document
+            .getElementById("yesNo")
+            .addEventListener('click', (e) => {
+                if (e.target.id === "yes") win.close(true);
+            })
+
+    })
+
+
 
     //Only launch new windows from the main window.
     if (win.identity.name === app.identity.uuid) {
@@ -31,4 +55,18 @@ async function init() {
             });
         });
     }
+}
+
+
+
+
+async function launchWindow() {
+    const popup = await fin.Window.create({
+        uuid: fin.Window.me.uuid,
+        name: `pop-up`,
+        url: `${location.origin}/popup.html`,
+        autoShow: true,
+        defaultCentered: true
+    })
+    return popup
 }
