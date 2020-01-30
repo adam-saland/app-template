75f9efc6ca74ed72344cae3792c0f53ae768b66b
adam-saland <adam.saland@openfin.co>
2020-01-30 09:10:52 -0500

    ngrx redux devtools implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { RemoteDevToolsProxy } from './remote-devtools-proxy';
 
+// Register our remote devtools if we're on-device and not in a browser
+if (!window['devToolsExtension'] && !window['__REDUX_DEVTOOLS_EXTENSION__']) {
+  const remoteDevToolsProxy = new RemoteDevToolsProxy({
+    connectTimeout: 300000, // extend for pauses during debugging
+    ackTimeout: 120000, // extend for pauses during debugging
+    secure: false // dev only
+  });
+
+  // support both the legacy and new keys, for now
+  window['devToolsExtension'] = remoteDevToolsProxy;
+  window['__REDUX_DEVTOOLS_EXTENSION__'] = remoteDevToolsProxy;
+  console.log(`window: `, window);
+}
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CounterComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
---
+++ src/app/remote-devtools-connection-proxy.ts
@@ -0,0 +1,26 @@
+import { ReduxDevtoolsExtensionConnection } from '@ngrx/store-devtools/src/extension';
+
+export class RemoteDevToolsConnectionProxy implements ReduxDevtoolsExtensionConnection {
+  constructor(public remotedev: any, public instanceId: string) {}
+  init() {}
+  error() {}
+  subscribe(listener: (change: any) => void): any {
+    const listenerWrapper = (change: any) => {
+      console.log(`change: `, change);
+      listener(change);
+    };
+
+    this.remotedev.subscribe(listenerWrapper);
+    // Hack fix for commit/time-travelling etc. if the devtools are already open
+    setTimeout(() => listenerWrapper({ type: 'START' }));
+  }
+  unsubscribe(): any {
+    // a lambda that calls it when their Observable wrapper is unsubscribed.
+    return () => this.remotedev.unsubscribe(this.instanceId);
+  }
+  send(action: any, state: any): any {
+    // Was commented has 'Not Called' but it's appear 
+    // we finally need this to send back responses to Redux DevTools
+    this.remotedev.send(action, state);
+  }
+}
