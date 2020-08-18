63cac5633447fbfcaaf4ec37dae69c9c59e20a11
adam-saland <adam.saland@openfin.co>
2020-08-18 09:59:32 -0400

    add btns/css

--- src/components/TitleBar/index.js
+++ src/components/TitleBar/index.js
@@ -6,6 +6,14 @@ export default function TitleBar() {
       <div className="title-bar-draggable">
         <div id="title" />
       </div>
+      <div id="buttons-wrapper">
+        <div className="button" title="Toggle Theme" id="theme-button" />
+        <div className="button" title="Toggle Sidebar" id="menu-button" />
+        <div className="button" title="Toggle Layout Lock" id="lock-button" />
+        <div className="button" title="Minimize Window" id="minimize-button" />
+        <div className="button" title="Maximize Window" id="expand-button" />
+        <div className="button" title="Close Window" id="close-button" />
+      </div>
     </div>
   );
 }
